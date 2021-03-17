8a9993840d24e69bdc2b98dfaab773394bd077c9
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-17 17:10:17 -0400

    Part 1, 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,17 +11,41 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allFeedback, setAll] = useState([]);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAll(allFeedback.concat(1));
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(allFeedback.concat(0));
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAll(allFeedback.concat(-1));
+  };
+
+  const averageFeedback = () => {
+    if(allFeedback.length === 0) return 0;
+    return allFeedback.reduce((a,b) => a + b, 0) / allFeedback.length;
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" handleClick={ () => setGood(good + 1) } />
-      <Button text="neutral" handleClick={ () => setNeutral(neutral + 1) } />
-      <Button text="bad" handleClick={ () => setBad(bad + 1) } />
+      <Button text="good" handleClick={ handleGoodClick } />
+      <Button text="neutral" handleClick={ handleNeutralClick } />
+      <Button text="bad" handleClick={ handleBadClick } />
       <h1>statistics</h1>
       <p>good { good }</p>
       <p>neutral { neutral }</p>
       <p>bad { bad }</p>
+      <p>all { allFeedback.length }</p>
+      <p>average { averageFeedback() }</p>
+      <p>positive { allFeedback.length > 0 ? (good / allFeedback.length)*100 : 0 } %</p>
     </div>
   )
 }
