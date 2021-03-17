8f1e0c8ef2e4e705dfc963b63082907f2ea30b93
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-17 15:51:02 -0400

    Part 1, 1.4

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -30,7 +30,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  const numExercises = props.exercises.reduce((a,b) => a + b, 0);
+  const numExercises = props.parts.reduce((a,b) => a + b.exercises, 0);
 
   return (
     <>
@@ -42,26 +42,26 @@ const Total = (props) => {
 const App = () => {
   const course = 'Half Stack application development';
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    }
+  ];
 
   return (
     <div>
       <Header course={ course } />
-      <Content parts={ [part1, part2, part3] } />
-      <Total exercises={ [part1.exercises, part2.exercises, part3.exercises] } />
+      <Content parts={ parts } />
+      <Total parts={ parts } />
     </div>
   )
 }
