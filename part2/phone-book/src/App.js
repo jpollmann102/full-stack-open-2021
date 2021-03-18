89229a5a5f765da9f998536aefbb539d29835f6b
Josh Pollmann <joshpollmann102@gmail.com>
2021-03-18 09:40:49 -0400

    Part 2, 2.11

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]);
+  const [ persons, setPersons ] = useState([]);
   const [ showPersons, setShowPersons ] = useState(persons);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ searchName, setSearchName ] = useState('');
+
+  const getPersons = () => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data);
+      setShowPersons(response.data);
+    });
+  }
+
+  useEffect(getPersons, []);
 
   const addPerson = (event) => {
     event.preventDefault();
